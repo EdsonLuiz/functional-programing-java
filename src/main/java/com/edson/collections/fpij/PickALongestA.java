dce520d8df40824980e88e8f113f8587280e564c
Edson Luiz Ribeiro Rodrigues <edsonrodrigues@icloud.com>
2022-05-14 19:23:42 -0300

    :memo: PickALongestA

---
+++ src/main/java/com/edson/collections/fpij/PickALongestA.java
@@ -0,0 +1,19 @@
+package com.edson.collections.fpij;
+
+import java.util.List;
+
+import com.edson.repo.GenericRepository;
+
+public class PickALongestA {
+  public static void main(String[] args) {
+    List<String> friends = GenericRepository.getFriends();
+    System.out.println("Total number of characteres in all names " + friends.stream()
+      .mapToInt(name -> name.length())
+      .sum());
+      // instead sum() we can use:
+      // max() to find the longest length
+      // min() to find tne shortest length
+      // sort() to sort the length
+      // average() to find the average of the length
+  }
+}
