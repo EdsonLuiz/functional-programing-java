13574e0d1508a5141de1675a6b34485d8a719655
Edson Luiz Ribeiro Rodrigues <edsonrodrigues@icloud.com>
2022-05-19 21:17:28 -0300

    :sparkles: more comparators

--- src/main/java/com/edson/compare/fpij/Compare.java
+++ src/main/java/com/edson/compare/fpij/Compare.java
@@ -1,5 +1,6 @@
 package com.edson.compare.fpij;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -31,5 +32,52 @@ class ReusingAComparator {
       GenericRepository.getPeople().stream()
         .sorted((person1, person2) -> person2.ageDifference(person1))
         .collect(Collectors.toList()));
+  }
+}
+
+class CompareV3 {
+  public static void main(String[] args) {
+    List<Person> people = GenericRepository.getPeople();
+
+    Comparator<Person> compareAscending = (person1, person2) -> person1.ageDifference(person2);
+    Comparator<Person> compareDescending = compareAscending.reversed();
+
+    Person.prinatPeople("Sorted in acending order", people.stream().sorted(compareAscending).collect(Collectors.toList()));
+
+    Person.prinatPeople("Sorted in descending order", people.stream().sorted(compareDescending).collect(Collectors.toList()));
+  }
+}
+
+class CompareV4 {
+  public static void main(String[] args) {
+    List<Person> people = GenericRepository.getPeople();
+
+    Comparator<Person> ascendingOrderByName = (person1, person2) -> person1.getName().compareTo(person2.getName());
+
+    Comparator<Person> descendingOrderByName = ascendingOrderByName.reversed();
+    
+    Person.prinatPeople("Sorted in ascending order by name", people.stream().sorted(ascendingOrderByName).collect(Collectors.toList()));
+
+    Person.prinatPeople("Sorted in descending order by name", people.stream().sorted(descendingOrderByName).collect(Collectors.toList()));
+  }
+}
+
+class CompareV5 {
+  public static void main(String[] args) {
+    List<Person> people = GenericRepository.getPeople();
+    
+    people.stream()
+      .min(Person::ageDifference)
+      .ifPresent(youngst -> System.out.println("Youngst" + youngst));
+  }
+}
+
+class CompareV6 {
+  public static void main(String[] args) {
+    List<Person> people = GenericRepository.getPeople();
+
+    people.stream()
+      .max(Person::ageDifference)
+      .ifPresent(eldest -> System.out.println("Eldest: " + eldest));
   }
 }
